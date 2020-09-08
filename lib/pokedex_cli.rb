49f430c6b4b312211be7df9ac960f6a0c1fa066d
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-09-08 13:23:35 -0400

    Tidied up the CLI menu and separated options into helper methods.  Provided looping options to keep the app going instead of closing at the end of each search.

--- lib/pokedex_cli.rb
+++ lib/pokedex_cli.rb
@@ -5,9 +5,9 @@ class PokedexCLI
 
     def initialize
         Pokedex.create
-        puts "  _---_   "
-        puts " |--O--|  "
-        puts "  ¯---¯   "
+        puts "  _---_                          _---_  "
+        puts " |--0--|  Pokemon Go Companion  |--0--| "
+        puts "  ¯---¯                          ¯---¯  "
         puts "Welcome, Trainer, to the Pokemon Go Companion app!"
         puts "If you want to be the very best, I can provide some helpful info."
     end
@@ -17,31 +17,26 @@ class PokedexCLI
         puts "  1. Find a Pokemon by name"
         puts "  2. Find a Pokemon by their Pokedex number"
         puts "  3. Find a Pokemon by type"
-        puts "  4. Show me the type chart"
+        puts "  4. Search type comparisons"
         puts "  To quit, type 'quit'."
         puts "Please enter your choice:"
         input = gets.chomp
 
         if input == "1"
-            puts "Please enter a name:"
-            Pokedex.find_by_name
+            name_search
+            exit_search
         end
+
         if input == "2"
-            puts "Please enter a Pokedex ID number:"
-            Pokedex.find_by_id
+            id_search
+            exit_search
         end
+
         if input == "3"
-            puts "Please enter a Pokemon Type:"
-            Pokedex.find_by_type
-            puts "You can search for another Pokemon type, type 'return' to go back, or 'quit' to quit."
-            input = gets.chomp
-            if input == "return"
-                self.call
-            end
-            if input == "quit"
-                puts "See you next time!"
-            end
+            type_search
+            exit_search
         end
+
         if input == "4"
             # Types.compare_types
         end
@@ -50,7 +45,46 @@ class PokedexCLI
             puts "See you next time!"
         end
         
-        
+    end
+
+    def name_search
+        puts "Please enter a name:"
+        Pokedex.find_by_name
+        search_again
+    end
+
+    def id_search
+        puts "Please enter a Pokedex ID number:"
+        Pokedex.find_by_id
+        search_again
+    end
+
+    def type_search
+        puts "Please enter a Pokemon Type:"
+        Pokedex.find_by_type
+        search_again
+    end
+
+    def search_again
+        puts "Would you like to search for another Pokemon? Y/N:"
+        input = gets.chomp.upcase
+        if input == "Y"
+            name_search
+        end
+        if input == "N"
+            puts "Alright, let's try something else!"
+        end
+    end
+
+    def exit_search
+        puts "Type 'return' to go back, or 'quit' to quit."
+        input = gets.chomp
+        if input == "return"
+            self.call
+        end
+        if input == "quit"
+            puts "See you next time!"
+        end
     end
 
 end
